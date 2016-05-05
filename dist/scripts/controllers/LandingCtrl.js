09fc8a8d70621af319d6ef5a3b3af073026d8481
Paco <palmtreerooskee@gmail.com>
2016-05-05 04:55:43 -0500

    merging macbook with imac 2

--- dist/scripts/controllers/LandingCtrl.js
+++ dist/scripts/controllers/LandingCtrl.js
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
-(function () {
-    function LandingCtrl() {    
-=======
+
 (function(){
     function LandingCtrl() {
        this.heroTitle = "Turn the Music Up!"
->>>>>>> d75c2ef9435de7b41dee3312c79057e58ab84a02
     } 
     
     angular
         .module('blocJams')
         .controller('LandingCtrl', LandingCtrl);
-<<<<<<< HEAD
-    
-=======
->>>>>>> d75c2ef9435de7b41dee3312c79057e58ab84a02
 })();
