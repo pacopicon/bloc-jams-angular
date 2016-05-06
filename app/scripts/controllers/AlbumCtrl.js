8265e41d94c5c1131d53f46206a63d2cae7c9fac
pacopicon <palmtreerooskee@gmail.com>
2016-05-06 04:11:00 -0500

    in the middle of checkpoint 6

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,31 @@
+//(function() {
+//    function AlbumCtrl() {
+//        this.albumData = [];
+//        this.albumData.push(angular.copy(albumPicasso));
+//        this.songs = albumPicasso.songs;
+//    }
+//    angular
+//        .module('blocJams')
+//        .controller('AlbumCtrl', AlbumCtrl)
+//})();
+
+//(function() {
+//    function AlbumCtrl(Fixtures) {
+//        this.albumData = Fixtures.getAlbum();
+//        this.songs = albumPicasso.songs;
+//    }
+//    angular
+//        .module('blocJams')
+//        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+//})();
+
+
 (function() {
-    function AlbumCtrl() {
-        this.albumData = [];
-        this.albumData.push(angular.copy(albumPicasso));
-        this.songs = albumPicasso.songs;
+    function AlbumCtrl(Fixtures) {
+        this.albumData = Fixtures.getAlbum();
+        this.songs = (Fixtures.getAlbum()).songs;
     }
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', AlbumCtrl)
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
